775942c7900c576540e50b84743b0da7792fb5f8
KoDaHyeon <wus2363@gmail.com>
2020-12-08 02:53:37 +0900

    Update Serial.ino

--- new/Serial/Serial.ino
+++ new/Serial/Serial.ino
@@ -2,7 +2,7 @@ int motorA1 =  5;
 int motorA2  = 6; 
 int motorB1 =  9; 
 int motorB2 =  10; 
-int speed = 170;
+int speed = 255;
 
 // initial command
 int command = 0;
@@ -36,56 +36,62 @@ if (Serial.available())
     analogWrite( motorA2 , 0); 
     analogWrite( motorB1 , speed); 
     analogWrite( motorB2 , 0);
-    Serial.println("On Track! Keep going!! GOGOGO");
+    //Serial.println("On Track! Keep going!! GOGOGO");
   }
 
    if (direction == 'l')
   {
   
-    analogWrite( motorA1 , speed);
+    analogWrite( motorA1 , 50);
     analogWrite( motorA2 , 0);
-    analogWrite( motorB1 , 0);
+    analogWrite( motorB1 , speed);
     analogWrite( motorB2 , 0);
-    Serial.println("Turn Left!");
+    //Serial.println("Turn Left!");
   }
 
    if (direction == 'L')
   {
   
-    analogWrite( motorA1 , speed);
-    analogWrite( motorA2 , 0);
-    analogWrite( motorB1 , 0);
-    analogWrite( motorB2 , 100);
-    Serial.println("Turn Left!!!!!!!!!");
+    analogWrite( motorA1 , 0);
+    analogWrite( motorA2 , speed);
+    analogWrite( motorB1 , speed);
+    analogWrite( motorB2 , 0);
+    //Serial.println("Turn Left!!!!!!!!!");
   }
 
    if (direction == 'r')
   {
   
-    analogWrite( motorA1 , 0);
+    analogWrite( motorA1 , speed);
     analogWrite( motorA2 , 0);
-    analogWrite( motorB1 , speed);
+    analogWrite( motorB1 , 50);
     analogWrite( motorB2 , 0);
-    Serial.println("Turn Right!");
+    //Serial.println("Turn Right!");
   }
 
   if (direction == 'R')
   {
-    analogWrite( motorA1 , 0);
-    analogWrite( motorA2 , 100);
-    analogWrite( motorB1 , speed);
-    analogWrite( motorB2 , 0);
-    Serial.println("Turn Right!!!!!!!");
+    analogWrite( motorA1 , speed);
+    analogWrite( motorA2 , 0);
+    analogWrite( motorB1 , 0);
+    analogWrite( motorB2 , speed);
+    //Serial.println("Turn Right!!!!!!!");
   }
 
   if (direction == 'B')
   {
     analogWrite( motorA1 , 0);
-    analogWrite( motorA2 , 100);
+    analogWrite( motorA2 , speed);
     analogWrite( motorB1 , 0);
-    analogWrite( motorB2 , 100);
-    Serial.println("Back!");
+    analogWrite( motorB2 , speed);
+    //Serial.println("Back!");
   }
+  delay(150);
+  analogWrite( motorA1 , 0);
+  analogWrite( motorA2 , 0);
+  analogWrite( motorB1 , 0);
+  analogWrite( motorB2 , 0);
+  Serial.write("ACK\n");
 
 }
 
