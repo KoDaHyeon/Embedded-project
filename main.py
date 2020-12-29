53c7f3d1f33660842f38b757813dceda6b5838ac
KoDaHyeon <wus2363@gmail.com>
2020-12-30 01:35:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,27 +9,25 @@ import picamera
 import picamera.array
 import time
 
-
-#N_SLICES만큼 이미지를 조각내서 Images[]배열에 담는다
+# N_SLICES만큼 이미지를 조각내서 Images[]배열에 담음
 Images = []
 N_SLICES = 10
-
 
 direction = ''
 
 print('start')
 time.sleep(3)
 
-
-
 for q in range(N_SLICES):
   Images.append(Image())
 
 camera = picamera.PiCamera()
-camera.resolution = (320, 240)
+camera.resolution = (320, 240) # 해상도
 camera.framerate = 30
-camera.brightness = 60
-camera.contrast = 65
+camera.brightness = 60 # 밝기 : 기본값은 50. 그림자를 제거하기 위해 밝기를 높임
+camera.contrast = 65 # 대비 : 기본값은 50. 그림자를 제거하기 위해 대비를 높임
+
+# 이미지의 중간 부분을 크롭해서 digital zoom
 zf = 0.2
 camera.zoom = (0+zf, 0+zf, 1-2*zf, 1-2*zf)
 rawCapture = picamera.array.PiRGBArray(camera, size = (320, 240))
@@ -37,20 +35,19 @@ time.sleep (0.1)
 
 
 for frame in camera.capture_continuous (rawCapture, format = "bgr", use_video_port = True):
-    # time.sleep(0.1)
     image = frame.array
     image = cv2.resize(image,(320,240))
 
-    # 이미지를 조각내서 윤곽선을 표시하게 무게중심 점을 얻는다
+    # 이미지를 조각내서 윤곽선을 표시하고 무게중심 점을 얻음
     Points = SlicePart(image, Images, N_SLICES)
-    #print('Points : ', Points)
 
-    # 조각난 이미지를 한 개로 합친다
+    # 조각난 이미지를 한 개로 합침
     fm = RepackImages(Images)
 
-
+	# 진행 방향을 계산해서 serial.ino 에 direction을 보냄
     Move(Points[0][0], Points[1][0], Points[2][0], Points[3][0], Points[4][0], Points[5][0], Points[6][0], Points[7][0], Points[8][0], Points[9][0])
-    #Display the resulting frame
+    
+    # 이미지를 화면에 출력
     cv2.imshow('frame', fm)
     rawCapture.truncate(0)
 
